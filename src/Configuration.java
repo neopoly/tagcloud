a5d24684ba059958055a57513da8670cbfbaa701
Jonas Thiel <jt@neopoly.de>
2013-03-11 17:06:52 +0100

    Update to new shape width

--- src/Configuration.java
+++ src/Configuration.java
@@ -2,7 +2,7 @@ public class Configuration {
     public static String shape_file  = "shape.png";
     public static String input_file  = "input.txt";
     public static String output_file = "out.png";
-    public static int width = 600;
+    public static int width = 500;
     public static int height = 636;
     public static int minSize = 8;
     public static int maxSize = 34;
