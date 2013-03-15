323115e65695f0b717788e7e9e05b2fc90b52de0
Jonas Thiel <jt@neopoly.de>
2013-03-15 11:51:18 +0100

    Document the run script

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+#
+# A shell script to use a tagcloud.jar on headless machines.
+# Stars a  Window Virtual Framebuffer for the run and kills it afterwards.
+# Machines with a frame buffer can use the following command directly
+# java -jar tagcloud.jar
+#
 APP_DIR=$(pwd)
 Xvfb :1 -screen 0 1024x768x24 &
 DISPLAY=:1 java -jar tagcloud.jar
